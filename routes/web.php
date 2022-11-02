f30c9594bda3e0323f8c457f9e05ba757e0827cf
Vaggelis Yfantis <me@octoper.me>
2022-11-03 01:10:07 +0200

    wip

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/redis', function () {
+    $visits = Cache::get('visits');
+    Cache::increment('visits');
+    return [
+        'counter' => $visits,
+    ];
 });
